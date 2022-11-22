915e5c2b896b66a5159c6e75c08be5aaaaafe694
Bruno Festino <brunomarcelo.festino@gmail.com>
2022-11-22 00:04:09 -0300

    adding provider package, folder structure

---
+++ lib/pages/places_list_page/places_list_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  const PlacesListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Places'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          )
+        ],
+      ),
+      body: Center(child: CircularProgressIndicator()),
+    );
+  }
+}
